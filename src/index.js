c9d04d88deaef7921e252aa0ccc0b5f4c48b975e
tho.bardot@gmail.com <tho.bardot@gmail.com>
2024-01-04 10:56:03 +0100

    Rename files and move styles in styles dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* istanbul ignore file */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
