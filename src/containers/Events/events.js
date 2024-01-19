83652e91bc80cb415d58ff367dd8ab0af0f7501f
tho.bardot@gmail.com <tho.bardot@gmail.com>
2024-01-19 14:57:14 +0100

    Debug categories select

--- src/containers/Events/events.js
+++ src/containers/Events/events.js
@@ -13,17 +13,14 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
-    (event, index) => {
-      if (
-        (currentPage - 1) * PER_PAGE <= index &&
-        PER_PAGE * currentPage > index
-      ) {
-        return true;
-      }
-      return false;
-    }
-  );
+
+  console.log("data?.events:", data?.events);
+  console.log("type:", type);
+
+  const filteredEvents = (data?.events || [])
+    .filter((event) => !type || event.type === type)
+    .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
